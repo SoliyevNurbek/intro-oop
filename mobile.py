7ea18e2303617b68239bd75473687e2e8bc07749
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2023-11-17 15:40:05 +0500

    Total methods

--- mobile.py
+++ mobile.py
@@ -8,12 +8,14 @@ class Mobile:
         self.quantity = quantity
 
     def info(self) -> str:
-        return f"{self.name} costs {self.price}"
+        pass
+    def Total(self) -> float:
+        return self.quantity*self.price
 
 
 
 m1 = Mobile('Samsung s21', 'black', 700.00, 6, 34)
 m2 = Mobile('Ihone 12 pro', 'gray', 600.00, 4, 21)
 
-print(m1.info())
-print(m2.info())
+print(m1.Total())
+print(m2.Total())
